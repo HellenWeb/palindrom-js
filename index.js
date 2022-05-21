2bc673d567b56c2f5aa5be498aa509d7056302c0
MacBook <a1@olesia.ospd.net>
2022-05-21 21:43:29 +1000

    Added changes in main.js file

--- index.js
+++ index.js
@@ -10,12 +10,8 @@ let uuid = require('uuid')
 
 // ** Create function "pal" with parameter "n"
 
-let pal = n => {
-    if (!n || typeof n !== 'string') return; // ** Doing a simple check
-    let arr = [ ...n ].reverse() // ! Expand the string into an array and reverse
-    console.log(n == arr.join('')); // ! Perform a comparison through "console.log"
-}
+let pal = (n = 1) => n === [...n].reverse().join("") 
 
-pal('ala')
+console.log(pal('ollo'))
 
 // || \\
